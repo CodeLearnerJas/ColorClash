d2ca5768e53a7bd0c7eb0091f8716a22d006d55c
CodeLearnerJas <yangtianjin2013@gmail.com>
2025-01-02 22:43:36 +1100

    Initial Commit

---
+++ ColorClash/ColorClashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorClashApp.swift
+//  ColorClash
+//
+//  Created by GuitarLearnerJas on 2/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorClashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
